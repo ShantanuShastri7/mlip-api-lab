9686b1be102fa680a758d89080da578d5ac925ca
Shantanu Shastri <shantanushastri70@gmail.com>
2026-01-15 05:10:36 -0500

    Implement a LiteLLm client to generate a structured JSON message that gives a travel itinerary for a destination given by the user.

--- analyze.py
+++ analyze.py
@@ -1,12 +1,14 @@
 import json
 import os
 from typing import Any, Dict
+from dotenv import load_dotenv
 from litellm import completion
 
 # You can replace these with other models as needed but this is the one we suggest for this lab.
 MODEL = "groq/llama-3.3-70b-versatile"
+load_dotenv()
 
-api_key = "hardcoded API_KEY HERE"
+# api_key = os.environ.get("LITELLM_API_KEY")
 
 
 def get_itinerary(destination: str) -> Dict[str, Any]:
@@ -21,7 +23,28 @@ def get_itinerary(destination: str) -> Dict[str, Any]:
 
     # See https://docs.litellm.ai/docs/ for reference.
 
-    data = ...
-    
+    data = completion(
+        model=MODEL,
+        messages=[
+                {
+                    "role": "system",
+                    # CRITICAL: Llama 3 requires explicit instruction to use JSON
+                    "content": "You are a helpful assistant. You must respond with valid JSON only. Do not add any markdown formatting like ```json ... ```."
+                },
+                {
+                    "role": "user",
+                    "content": f"Extract a structured travel itinerary details from this text into JSON format, the JSON should only include destination, price_range, ideal_visit_times, and top_attractions: {destination}"
+                }
+            ],
+            # 3. Enable JSON mode
+            response_format={"type": "json_object"}, 
+            temperature=0.1
+    )    
+
+    content_str = data.choices[0].message.content
+
+    # 2. Parse the string into a Python Dictionary
+    # Since you requested JSON mode, this string is valid JSON.
+    data = json.loads(content_str)
 
     return data
